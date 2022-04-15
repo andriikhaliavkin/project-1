6c2a78a2d352e3df25d48e4007349083fea0516a
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-04-15 10:49:09 +0200

    client-server app

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-console.log("Hello world!")
-console.log(12)
-console.log(1.9)
-console.log(typeof 1)
-console.log(typeof 2.0)
-console.log(1 - 4)
-console.log(typeof (3 / 4))
-console.log(2 ** 3)
-console.log(5 % 2)
-console.log(9 / 3 | 0) 
+const http = require('http') // подключение CommonJS module http
+const fs = require('fs') // подключения CommonJS module fs  (файловая система)
+const server = http.createServer(handleRequest) // создаем сервер
+server.listen(3000) // запускаем сервер на порту 3000
+
+function handleRequest(request, response) {           // объявляем функцию обработки запросов
+  response.end(fs.readFileSync('public/index.html'))  // даем ответ клиенту
+}
